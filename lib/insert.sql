81bdf15c68fe48b604bc3d91ba7f44a933bbae18
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-17 04:02:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,21 +1,4 @@
-INSERT INTO users (name, age) VALUES
-('BJ', 33);
-('Cashew', 10);
-('Peanut', 15);
-('Agnes', 50);
-('Ranem', 31);
-('Nick', 32);
-('Joe', 59);
-('Melissa', 37);
-('Frank', 60);
-('Josie', 58);
-('Frankie', 13);
-('Rashid', 14);
-('Emmanuel', 45);
-('Martin', 38);
-('Monique', 28);
-('Michelle', 39);
-('Darryn', 18);
-('Scott', 29);
-('Benjamin', 46);
-('Rachel', 34);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
